a4f34aeac0b1bc62e1f524b672c43ae293931b17
enric.macias.lopez <enric.macias.lopez@glpgs.com>
2015-05-15 17:55:06 +0900

    First commit

---
+++ Example/Pods/Target Support Files/Pods-EMLMenuBar/Pods-EMLMenuBar-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// EMLMenuBar
+#define COCOAPODS_POD_AVAILABLE_EMLMenuBar
+#define COCOAPODS_VERSION_MAJOR_EMLMenuBar 0
+#define COCOAPODS_VERSION_MINOR_EMLMenuBar 1
+#define COCOAPODS_VERSION_PATCH_EMLMenuBar 0
+
