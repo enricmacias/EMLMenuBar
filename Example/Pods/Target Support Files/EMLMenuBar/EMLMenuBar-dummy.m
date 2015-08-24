eb89d751ffd8fb99ca764b37ba9e4b66b3ba99c5
enric.macias.lopez <enric.macias.lopez@glpgs.com>
2015-08-24 16:06:50 +0900

    Updates all pods to the latest version.

---
+++ Example/Pods/Target Support Files/EMLMenuBar/EMLMenuBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EMLMenuBar : NSObject
+@end
+@implementation PodsDummy_EMLMenuBar
+@end
--- Example/Pods/Target Support Files/Pods-EMLMenuBar-EMLMenuBar/Pods-EMLMenuBar-EMLMenuBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EMLMenuBar_EMLMenuBar : NSObject
-@end
-@implementation PodsDummy_Pods_EMLMenuBar_EMLMenuBar
-@end
